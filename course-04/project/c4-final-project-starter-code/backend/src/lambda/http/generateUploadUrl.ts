747a3a55690b8e3aad79c6c9bf8dce0e99105d0c
sglasser <steve@steveglasser.com>
2019-12-25 14:32:43 -0600

    added file upload

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
@@ -1,12 +1,31 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS from 'aws-sdk'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
 
-  console.log(todoId)
+  const s3 = new AWS.S3({
+    signatureVersion: 'v4'
+  });
 
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+  const url = s3.getSignedUrl('putObject', {
+    Bucket: process.env.TODOS_S3_BUCKET,
+    Key: todoId,
+    Expires: process.env.SIGNED_URL_EXPIRATION
+  })
+
+  console.log('upload url', url);
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      uploadUrl: url
+    })
+  }
 }
